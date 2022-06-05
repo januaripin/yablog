a803a328aa1e940c37272449c557d1fddc3f4908
Yanuar Arifin <halo@yanuar.id>
2022-06-05 12:30:57 +0700

    add app bar logo

--- lib/presentation/main/main_scaffold.dart
+++ lib/presentation/main/main_scaffold.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import '../../const/assets_path.dart';
 import '../../router/app_router_state.dart';
 import '../../utils/app_util.dart';
+import '../../utils/gap.dart';
 import '../app/app_themes.dart';
 
 class MainScaffold extends StatelessWidget {
@@ -16,9 +18,16 @@ class MainScaffold extends StatelessWidget {
       appBar: AppBar(
         title: GestureDetector(
           onTap: () => AppRouterState().goToHome(),
-          child: Text(
-            AppUtil.title,
-            style: AppThemes.textTheme.titleLarge,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(AssetsPath.abLogo, width: 32),
+              Gap.p8(),
+              Text(
+                AppUtil.title,
+                style: AppThemes.textTheme.titleLarge,
+              ),
+            ],
           ),
         ),
       ),
